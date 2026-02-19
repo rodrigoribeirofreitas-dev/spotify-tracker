3b1f4068304187e6564bc62e13ef7441ec2ec2a9
rodrigoribeirofreitas-dev <rodrigoribeirofreitas@gmail.com>
2026-02-19 15:28:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ CID = 'bf24024ba81d409c9af3ce7ca8f95c3f'
 CSEC = '0ced5b2211c5471ca53c3fe938aa3ba3'
 
 # O código que você acabou de me enviar (Fresco)
-CODIGO_NOVO = 'AQCBw6ePqONl-QKFhJcSQ8Fjkok1jInizEN8ipVq3ZwKt6rlM2DkBEU-0PmF1UJA3IDIYAuKReVN1aNtQSB7dBu-aNhd2QHyY_oK4WR58kU71JFUjdzuCuBXnvzhvVS3HCk38Xlm7XJyAJbHtigHh-s0lu-44sGQEw6reMP9rsnwUJOkbgz32jkmy3YVQOJEZsPLU2H_Ba2A9hk76QUgYXvKhg-cZLhodVJuYjRFXMdqyB2Q9JH1EpPXGauEzrLM_A'
+CODIGO_NOVO = 'AQCL-gWlNbAW9W6XV0mGIHNluJ6dqr-iUfLHFG9_hjB7ZGW3AUlV2vXJB5gGM69kwiUmPCQrYjVcti_KbOxBtL_uGGSRWntoT6h5hoIpXvG0Tz-Ur9qUp7S--iJZ41kfqL0'
 
 def obter_chave_eterna():
     try:
